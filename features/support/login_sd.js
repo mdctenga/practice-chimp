e929f2f824545dd6c2dc1b542cb63cb745749c10
mdctenga <mdctenga@gmail.com>
2016-01-23 10:37:21 -1000

    Change static text to REGEX

--- features/support/login_sd.js
+++ features/support/login_sd.js
@@ -1,5 +1,27 @@
 module.exports = function () {
 
+  // Jason's attempt
+  // this.Given(/^I navigate to Facebook$/, function () {
+  // client.url('http://facebook.com');
+  // });
+
+  // this.Given(/^Input "([^"]*)"$/, function (username) {
+  // client
+  //   .setValue('input[name="email"]', username );
+  // });
+
+  // this.Given(/^Input "([^"]*)"$/, function (password) {
+  // client
+  //   .setValue('input[name="pass"]', password);
+  // });
+
+  // this.When(/^I click "([^"]*)"$/, function () {
+  // // Write the automation code here
+  // // client.click('#u_0_n');
+  // pending();
+  // });
+
+
   this.Given(/^I navigate to Facebook$/, function () {
     client.url('http://Facebook.com');
   });
@@ -14,7 +36,7 @@ module.exports = function () {
       .setValue('input[name="pass"]', password);
   });
 
-  this.Given(/^I click Log In button$/, function () {
+  this.Given(/^I can click "([^"]*)"$/, function (login) {
     client
       .leftClick('#u_0_n');
   });
